e238b30a6194e314d2bcb00c3298875be689a097
null8bit <duneohx@gmail.com>
2023-02-27 17:43:11 -0300

    new file:   src.rar

--- src/mongo/db.js
+++
@@ -1,8 +0,0 @@
-var mongo = require("mongoose");
-var dotenv = require('dotenv');
-
-dotenv.config();
-mongo.set('strictQuery', false);
-var db = mongo.connect(process.env['MONGO_URL'] + process.env['DB_NAME']);
-
-module.exports = db
--- src/utils/gen_id.js
+++
@@ -1,7 +0,0 @@
-var crypto = require("crypto")
-module.exports = function ({username, password, createdAt}) {
-    var plainTxtString = `${createdAt}:${username}:${password}:NULL8BITEST`
-    var md5String = crypto.createHash('md5').update(plainTxtString).digest('hex')
-    
-    return md5String
-}
