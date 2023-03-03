b4298469f1a9786a7861766b4a1e5eac2be627de
null8bit <duneohx@gmail.com>
2023-03-03 10:17:56 -0300

    modified:   src/routes/middlewares/crud.js 	new file:   src/routes/middlewares/private-routes.json 	new file:   src/routes/middlewares/userController.js 	modified:   src/routes/middlewares/utils.js

--- src/routes/middlewares/crud.js
+++ src/routes/middlewares/crud.js
@@ -1,10 +1,8 @@
 // USER HANDLER FUNCTIONS
 var path = require("path");
 var debug = require("debug")("app:crud"); // DEBUG LINE
-var {User} = require(path.resolve(__dirname, '..', '..', 'mongo', 'models'))
-var {gen_id} = require('./utils');
+var {gen_id, findUser, jwt_utils} = require('./utils');
 var messages = require("./messages.json");
-var jwt = require("jsonwebtoken");
 
 module.exports = {
     register: function (req, res) {
@@ -36,35 +34,4 @@ module.exports = {
     update: function (req, res) {},
     delete: function (req, res) {},
     info: function (req, res) {},
-}
-var findUser = (credentials) => new Promise((resolve, reject) => {
-    User.findOne({username:credentials['username']}).where('password').eq(credentials['password'])
-    .exec(function (err, userdata) {
-        if (!err) {
-            userdata = {
-                username: userdata['username'],
-                user_id: userdata['user_id'],
-                email: userdata['email']
-            }
-            resolve(userdata)
-        } else {
-            reject(err)
-        }
-    })
-})
-
-var jwt_utils = {
-    verifyToken: function (token) {
-
-    }, 
-    genToken: (data) => new Promise((resolve, reject) => {
-        jwt.sign({
-            exp: Math.floor(Date.now() / 1000) + (60 * 60), data
-          }, process.env['PRIVATE_KEY'], {
-            algorithm: 'HS256'
-          }, (err, token) => {
-            if(!err) resolve(token)
-            else reject(err)
-          });
-    })
 }
--- src/routes/middlewares/utils.js
+++ src/routes/middlewares/utils.js
@@ -1,5 +1,37 @@
 var crypto = require("crypto")
+var {User} = require(path.resolve(__dirname, '..', '..', 'mongo', 'models'))
+var jwt = require("jsonwebtoken");  
 module.exports = {
+    findUser: (credentials) => new Promise((resolve, reject) => {
+        User.findOne({username:credentials['username']}).where('password').eq(credentials['password'])
+        .exec(function (err, userdata) {
+            if (!err) {
+                userdata = {
+                    username: userdata['username'],
+                    user_id: userdata['user_id'],
+                    email: userdata['email']
+                }
+                resolve(userdata)
+            } else {
+                reject(err)
+            }
+        })
+    }),
+    jwt_utils: {
+        verifyToken: function (token) {
+    
+        }, 
+        genToken: (data) => new Promise((resolve, reject) => {
+            jwt.sign({
+                exp: Math.floor(Date.now() / 1000) + (60 * 60), data
+              }, process.env['PRIVATE_KEY'], {
+                algorithm: 'HS256'
+              }, (err, token) => {
+                if(!err) resolve(token)
+                else reject(err)
+              });
+        })
+    },
     gen_id: (username) => {
         var plainTextId = `${username}:${Date.now()}:null8bit6`;
         var hashedId = crypto.createHash('md5').update(plainTextId).digest('hex')
