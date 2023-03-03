efbd84e4542ecff26365c35a1769f9f9df2a25e2
null8bit <duneohx@gmail.com>
2023-03-03 10:07:03 -0300

    modified:   .gitignore 	deleted:    private/gen-keys.js

--- private/gen-keys.js
+++
@@ -1,8 +0,0 @@
-var crypto = require("crypto")
-var fs = require('fs')
-
-var {publicKey, privateKey} =  crypto.generateKeyPairSync('rsa', {
-    modulusLength: 2048
-})
-
-fs.writeFileSync('priv.pem', privateKey.export({type: 'pkcs1', format: 'pem'}))
